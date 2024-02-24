0d00a089086e5f835578cfc4d37707af30d3fc23
RadiALo <78644882+RadiALo@users.noreply.github.com>
2024-02-24 13:45:51 +0200

    Initial commit

---
+++ src/main/java/org/radialo/spigotprivatemessages/PrivateMessagesPlugin.java
@@ -0,0 +1,17 @@
+package org.radialo.spigotprivatemessages;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class PrivateMessagesPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
