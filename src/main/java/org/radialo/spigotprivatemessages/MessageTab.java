c4f1de58d76e6de331f2c525687b4f4e85bd8bdc
RadiALo <78644882+RadiALo@users.noreply.github.com>
2024-02-24 14:34:40 +0200

    Better autocomplete

--- src/main/java/org/radialo/spigotprivatemessages/MessageTab.java
+++ src/main/java/org/radialo/spigotprivatemessages/MessageTab.java
@@ -5,7 +5,9 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabCompleter;
 import org.bukkit.entity.Player;
+import org.bukkit.util.StringUtil;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -13,12 +15,16 @@ public class MessageTab implements TabCompleter {
     @Override
     public List<String> onTabComplete(CommandSender sender, Command command, String label, String[] args) {
         if (args.length == 1) {
-            return Bukkit.getOnlinePlayers()
-                    .stream()
-                    .map(Player::getName)
-                    .collect(Collectors.toList());
+            return StringUtil.copyPartialMatches(
+                    args[0],
+                    Bukkit.getOnlinePlayers()
+                            .stream()
+                            .map(Player::getName)
+                            .collect(Collectors.toList()),
+                    new ArrayList<>()
+            );
+        } else {
+            return new ArrayList<>();
         }
-
-        return null;
     }
 }
